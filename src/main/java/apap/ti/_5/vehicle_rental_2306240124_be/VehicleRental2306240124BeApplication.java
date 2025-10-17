fc31478defdda3d6e0a193d1164894d6fb6a174c
nairafiany <naira.afiany@gmail.com>
2025-10-17 11:16:35 +0700

    initialize repository

---
+++ src/main/java/apap/ti/_5/vehicle_rental_2306240124_be/VehicleRental2306240124BeApplication.java
@@ -0,0 +1,13 @@
+package apap.ti._5.vehicle_rental_2306240124_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRental2306240124BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRental2306240124BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/_5/vehicle_rental_2306240124_be/VehicleRental2306240124BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.vehicle_rental_2306240124_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRental2306240124BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
